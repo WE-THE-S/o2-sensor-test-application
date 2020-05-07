7db6b170cba2e0ef805593b9303a105d0038ba19
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-07 14:24:58 +0900

    material layout 적용하면서 발생한 버그 수정
    material layout으로 래핑하면서 text 변수에 바로 접근이 불가능한 문제 수정

--- app/src/main/java/kr/thes/o2/activity/user/SetUserInformationActivity.kt
+++ app/src/main/java/kr/thes/o2/activity/user/SetUserInformationActivity.kt
@@ -2,6 +2,7 @@ package kr.thes.o2.activity.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_set_user_information.*
 import kr.thes.o2.R
 import kr.thes.o2.utils.setSharedString
@@ -13,15 +14,15 @@ class SetUserInformationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_user_information)
         ok_button.setOnClickListener {
-            if(user_blood_type.text.toString().isNotBlank() and user_name.text.toString().isNotBlank() and
-                user_phone_number.text.toString().isNotBlank() and admin_name.text.toString().isNotBlank() and
-                admin_phone_number.text.toString().isNotBlank()){
+            if(user_blood_type.editText!!.text.toString().isNotBlank() and user_name.editText!!.text.toString().isNotBlank() and
+                user_phone_number.editText!!.text.toString().isNotBlank() and admin_name.editText!!.text.toString().isNotBlank() and
+                admin_phone_number.editText!!.text.toString().isNotBlank()){
                 val list = arrayListOf(
-                    Pair("user_name", user_name.text.toString()),
-                    Pair("user_blood_type", user_blood_type.text.toString()),
-                    Pair("user_phone_number", user_phone_number.text.toString()),
-                    Pair("admin_name", admin_name.text.toString()),
-                    Pair("admin_phone_number", admin_phone_number.text.toString()),
+                    Pair("user_name", user_name.editText!!.text.toString()),
+                    Pair("user_blood_type", user_blood_type.editText!!.text.toString()),
+                    Pair("user_phone_number", user_phone_number.editText!!.text.toString()),
+                    Pair("admin_name", admin_name.editText!!.text.toString()),
+                    Pair("admin_phone_number", admin_phone_number.editText!!.text.toString()),
                     Pair("user_type", "user")
                 )
                 list.forEach {
