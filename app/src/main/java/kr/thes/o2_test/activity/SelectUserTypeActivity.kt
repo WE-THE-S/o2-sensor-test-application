9c2dd0b8cc70adb83354c6203cfb5e89c243d9e4
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-07 10:10:23 +0900

    add select user type activity

---
+++ app/src/main/java/kr/thes/o2_test/activity/SelectUserTypeActivity.kt
@@ -0,0 +1,13 @@
+package kr.thes.o2_test.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.thes.o2_test.R
+
+
+class SelectUserTypeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_user_type)
+    }
+}
